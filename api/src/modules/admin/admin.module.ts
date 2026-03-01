2d7b5705c3f4eb235d36134397addc4d3e513ee3
smignacco <sebastien@mignacco.com>
2026-03-01 11:28:42 -0500

    Améliorer le courriel de relance en HTML professionnel Cisco

--- api/src/modules/admin/admin.module.ts
+++ api/src/modules/admin/admin.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { WebexModule } from '../webex/webex.module';
+import { ReminderModule } from '../reminder/reminder.module';
 import { AdminController } from './admin.controller';
 
-@Module({ imports: [JwtModule.register({}), WebexModule], controllers: [AdminController] })
+@Module({ imports: [JwtModule.register({}), WebexModule, ReminderModule], controllers: [AdminController] })
 export class AdminModule {}
